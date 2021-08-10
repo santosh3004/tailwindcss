0a5ba01e18080ce3c0f3d19cae79c232b6361734
santosh3004 <msantoaubedi12@gmail.com>
2021-08-10 13:53:43 +0545

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        primary:'#FF6363',
+        secondary:{
+          100:'#E2E2D5',
+          200:'#888885'
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
